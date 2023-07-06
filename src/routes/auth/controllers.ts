ffa694bc8141928868565f124ba8c91f61ffd929
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2023-07-05 22:56:22 -0600

    refactor responses

--- src/routes/auth/controllers.ts
+++ src/routes/auth/controllers.ts
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 const secretKey = "PEPE_PICA_PAPAS_SECRET"
 
 const generateTokens = (id: number) =>{
-  const accessToken: string = jwt.sign({ id: id }, secretKey, { expiresIn: '1m' });
+  const accessToken: string = jwt.sign({ id: id }, secretKey, { expiresIn: '1hr' });
   const refreshToken: string = jwt.sign({ id: id }, secretKey, { expiresIn: '30d' });
   return { accessToken, refreshToken };
 }
@@ -31,14 +31,14 @@ const findUserById =async  (id: number) => {
 export const authenticationToken = async (req: IGetUserAuthInfoRequest, res: Response, next: () => any) =>{
   const token = req.headers['authorization']?.split(" ")[1];
   if (!token) {
-    return res.status(401).json({ error: 'Unauthorized' });
+    return res.status(401).json({ status: "error", message: 'Unauthorized' });
   }
 
   try {
     const decoded = jwt.verify(token, secretKey) as { id: number }; 
     
     if (!decoded){
-      return res.status(401).json({ error: 'Unauthorized' });
+      return res.status(401).json({ status: "error", message: 'Unauthorized' });
     }
 
     const user = await findUserById(decoded.id)
@@ -56,20 +56,20 @@ export const signIn = async (req: Request<{},{},TUserAuth>, res: Response) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({ error: 'Missing required parameter' });
+    return res.status(400).json({ status: "error", message: 'Missing required parameter' });
   }
 
   if (!validateEmail(email)) {
-    return res.status(422).json({ error: 'Invalid email address' });
+    return res.status(422).json({ status: "error", message: 'Invalid email address' });
   }
 
   if (!validatePassword(password)) {
-    return res.status(400).json({ message: 'The password does not meet the security criteria.' });
+    return res.status(400).json({ status: "error", message: 'The password does not meet the security criteria.' });
   }
 
   const emailExist = await findUserByEmail(email);
   if (emailExist) {
-    return res.status(409).json({ message: 'This email is already in use' });
+    return res.status(409).json({ status: "error", message: 'This email is already in use' });
   }
 
   try {
@@ -86,13 +86,18 @@ export const signIn = async (req: Request<{},{},TUserAuth>, res: Response) => {
 
     const {password: _, ...userWithoutPassword} = user as TUser;
 
-    return res.status(200).json({
-      ...userWithoutPassword,
-      accessToken
+    return res.status(200).json({ 
+      status: "success", 
+      data: {
+        user: {
+          ...userWithoutPassword,
+          accessToken
+        }
+      }
     });
   } catch (error) {
     console.error('Error occurred during sign in:', error);
-    return res.status(500).json({ error: 'Internal server error' });
+    return res.status(500).json({ status: "error", message: 'Internal server error' });
   }
 };
 
@@ -102,22 +107,22 @@ export const login = async (req: Request<{}, {}, TUserAuth>, res: Response) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({ error: 'Missing required parameter' });
+    return res.status(400).json({ status: "error", message: 'Missing required parameter' });
   }
 
   if (!validateEmail(email)) {
-    return res.status(422).json({ error: 'Invalid email address' });
+    return res.status(422).json({ status: "error", message: 'Invalid email address' });
   }
 
   try {
     const user = await findUserByEmail(email);
     if (!user) {
-      return res.status(401).json({ error: 'Invalid email or password' });
+      return res.status(401).json({ status: "error", message: 'Invalid email or password' });
     }
 
     const passwordMatch = await comparePassword(password, user.password);
     if (!passwordMatch) {
-      return res.status(401).json({ error: 'Invalid email or password' });
+      return res.status(401).json({ status: "error", message: 'Invalid email or password' });
     }
 
     const { refreshToken, accessToken } = generateTokens(user.id);
@@ -127,13 +132,18 @@ export const login = async (req: Request<{}, {}, TUserAuth>, res: Response) => {
     const { password: _, ...userWithoutPassword } = user;
 
     return res.status(200).json({
-      ...userWithoutPassword,
-      refreshToken,
-      accessToken
+      status: "success",
+      data: {
+        user: {
+            ...userWithoutPassword,
+            refreshToken,
+            accessToken
+        }
+      }
     });
   } catch (error) {
     console.error('Error occurred during login:', error);
-    return res.status(500).json({ error: 'Internal server error' });
+    return res.status(500).json({ status: "error", message: 'Internal server error' });
   }
 };
 
@@ -143,11 +153,11 @@ export const refreshToken = async (req: IGetUserAuthInfoRequest, res: Response) 
   const refreshToken = req.headers['authorization']?.split(" ")[1];
 
   if (!refreshToken) {
-    return res.status(401).json({ error: 'Unauthorized' });
+    return res.status(401).json({ status: "error", message: 'Unauthorized' });
   }
 
   if (refreshToken !== req.user?.refreshToken) {
-    return res.status(401).json({ error: 'Unauthorized' });
+    return res.status(401).json({ status: "error", message: 'Unauthorized' });
   }
 
   try {
@@ -157,13 +167,16 @@ export const refreshToken = async (req: IGetUserAuthInfoRequest, res: Response) 
 
     await pool.query('UPDATE users SET refreshToken = ? WHERE id = ?', [newRefreshToken, decoded.id]);
 
-    return res.status(200).json({
-      refreshToken: newRefreshToken,
-      accessToken
+    return res.status(200).json({ 
+      status: "success", 
+      data: {
+        refreshToken: newRefreshToken,
+        accessToken
+      }
     });
   } catch (error) {
     console.error('Error occurred during token refresh:', error);
-    return res.status(500).json({ error: 'Internal server error' });
+    return res.status(500).json({ status: "error", message: 'Internal server error' });
   }
 };
 
